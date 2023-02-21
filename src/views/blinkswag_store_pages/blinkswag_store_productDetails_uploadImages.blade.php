e78c709f683183bc4f5fbf14ff7fbd4e4d63aafa
Naseer Ali Soomro <naseerali.soomro@basecamppk.com>
2023-02-21 05:27:01 -0500

    some modifications

--- src/views/blinkswag_store_pages/blinkswag_store_productDetails_uploadImages.blade.php
+++ src/views/blinkswag_store_pages/blinkswag_store_productDetails_uploadImages.blade.php
@@ -79,7 +79,7 @@
                 ?>
                 @foreach($product_details['product']['files'] as $key=>$placement)
                     @if($placement['type']!="mockup" && $placement['type']!="embroidery_chest_left" && $placement['type']!="embroidery_chest_center" )
-                    <li class="nav-item selected_item" style="font-wieght: 600">
+                    <li class="nav-item selected_item">
                         <a class="nav-link {{$isactive}}" id="design_layout_{{$placement['type']}}-tab" data-toggle="tab" href="#design_layout_{{$placement['type']}}" role="tab" aria-selected="true">{{$placement['title']}}</a>
                     </li>
                     <?php
@@ -95,7 +95,7 @@
                 @foreach($product_details['product']['files'] as $key=>$placement)
                     @if( $placement['type']!="mockup" && $placement['type']!="embroidery_chest_left" && $placement['type']!="embroidery_chest_center" )
                         <div class="tab-pane fade show m-3 {{$isactive}}" id="design_layout_{{$placement['type']}}" role="tabpanel" aria-labelledby="design_layout_{{$placement['type']}}-tab">
-                            {{-- {{$placement['type']}} --}}
+                            {{$placement['type']}}
 
                             <div class="dropzone-wrapper gallery_wrapper">
                                 <div class="preview-zone hidden">
@@ -113,8 +113,8 @@
                             </div>
 
                             <div class="shirtDiv_{{$placement['type']}}" class="page"
-                                style="width: 530px; position: relative; background-color: rgb(255, 255, 255);">
-                                <img class="js-qv-product-cover card-img-top product_image"  alt="">
+                                style="width: 530px; height: 530px; position: relative; background-color: rgb(255, 255, 255);">
+                                <img class="js-qv-product-cover card-img-top product_image"  alt="Card image cap">
                             </div>
                         </div>
                         <?php
@@ -128,7 +128,7 @@
 
     <div class="row">
         <div class="col-md-12 text-right">
-            <a class="nav-link btn btn-default saveproduct w-25" style="color: white !important; margin:auto; display:none">Save</a>
+            <a class="nav-link btn btn-default saveproduct w-25 mb-2" style="color: white !important; margin:auto; display:none">Save</a>
         </div>
     </div>
 
@@ -140,139 +140,128 @@
  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-jcrop/0.9.15/js/jquery.Jcrop.js"></script>
 
  <script>
-var _token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-$(document).ready(function(){
-    //wsmemon
-    $(document).on("change",".dropzone", function(e){
-            //readFile(this);
-        });
-        $(document).on("dragover",".dropzone-wrapper", function(e){
-            e.preventDefault();
-            e.stopPropagation();
-            $(this).addClass('dragover');
-        });
-        $(document).on("dragleave",".dropzone-wrapper", function(e){
-            e.preventDefault();
-            e.stopPropagation();
-            $(this).removeClass('dragover');
-        });
+    var _token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+    $(document).ready(function(){
+        //wsmemon
+        $(document).on("change",".dropzone", function(e){
+                //readFile(this);
+            });
+            $(document).on("dragover",".dropzone-wrapper", function(e){
+                e.preventDefault();
+                e.stopPropagation();
+                $(this).addClass('dragover');
+            });
+            $(document).on("dragleave",".dropzone-wrapper", function(e){
+                e.preventDefault();
+                e.stopPropagation();
+                $(this).removeClass('dragover');
+            });
+            window.image_path = "{{url('/')}}"+"/Public/Image/Transparent Images/"+"{{$product_id}}";
+            function readURL_gallery(input) {
+                if (input.files && input.files[0]) {
+                    var myimage = $(input).prop("files")[0];
+                    var filename = $(input).attr("filename");
+                    var fd = new FormData();
+                    fd.append("myimage", myimage);
+                    fd.append("product_id", "{{$product_id}}");
+                    fd.append("filename", filename);
+                    $.ajaxSetup({
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        }
+                    });
+                    $.ajax({
+                        url: "{{$domain_name}}/uploadimage_printful",
+                        type: "POST",
+                        data:fd,
+                        contentType: false,
+                        cache: false,
+                        processData: false,
+                        success: function(data) {
+                            data = JSON.parse(data);
+                            if(data.status=="success")
+                            {
+                               if($(input).parents(".dropzone-wrapper:first").next().find(".jcrop-holder").length>0)
+                               {
+                                    $(input).parents(".dropzone-wrapper:first").next().find("img.product_image:first").data("Jcrop").destroy();
+                               }
+                                $(input).parents(".dropzone-wrapper:first").next().find("img.product_image:first").attr("src",image_path+`/`+data.filename);
+                                $(input).parents(".dropzone-wrapper:first").next().find("img.product_image:first").Jcrop({
+                                    onSelect: function(c){
+                                        console.log(c);
+                                        $("img.product_image:visible").attr("inner_canvas", JSON.stringify(c));
 
-        window.image_path = "{{url('/')}}"+"/Public/Image/Transparent Images/"+"{{$product_id}}";
-        function readURL_gallery(input) {
-            if (input.files && input.files[0]) {
-
-                var myimage = $(input).prop("files")[0];
-                var filename = $(input).attr("filename");
-
-                var fd = new FormData();
-                fd.append("myimage", myimage);
-                fd.append("product_id", "{{$product_id}}");
-                fd.append("filename", filename);
-
-                $.ajaxSetup({
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    }
-                });
-                $.ajax({
-                    url: "{{$domain_name}}/uploadimage_printful",
-                    type: "POST",
-                    data:fd,
-                    contentType: false,
-                    cache: false,
-                    processData: false,
-                    success: function(data) {
-                        data = JSON.parse(data);
-                        if(data.status=="success")
-                        {
-                           if($(input).parents(".dropzone-wrapper:first").next().find(".jcrop-holder").length>0)
-                           {
-                                $(input).parents(".dropzone-wrapper:first").next().find("img.product_image:first").data("Jcrop").destroy();
-                           }
-
-                            $(input).parents(".dropzone-wrapper:first").next().find("img.product_image:first").attr("src",image_path+`/`+data.filename);
-                            $(input).parents(".dropzone-wrapper:first").next().find("img.product_image:first").Jcrop({
-                                onSelect: function(c){
-                                    console.log(c);
-                                    $("img.product_image:visible").attr("inner_canvas", JSON.stringify(c));
-
-                                    if($(".jcrop-holder div div .jcrop-tracker").length == $(".mockup_tabs li").length)
+                                        if($(".jcrop-holder div div .jcrop-tracker").length == $(".mockup_tabs li").length)
                                     {
                                         $("a.saveproduct").show();
                                     }else{
                                        $("a.saveproduct").hide();
                                     }
 
-                                }
-                            },function(){
-                                $(".jcrop-holder").css("background-color","white");
-                                } );
-
-
-
+                                    }
+                                },function(){
+                                    $(".jcrop-holder").css("background-color","white");
+                                    } );
+                            }
+                        }
+                    });
+                }
+            }
+            $(".gallery").change(function(){
+                readURL_gallery(this);
+            });
+            $(document).on("click", ".saveproduct", function(e){
+                var complete_json = {};
+                $(".jcrop-holder img.js-qv-product-cover.product_image").each(function(index, value){
+                    var src = $(value).attr("src");
+                    var inner_canvas = $(value).attr("inner_canvas");
+                    var arr1 = {};
+                    arr1['src'] = src;
+                    arr1['inner_canvas'] = inner_canvas;
+                    complete_json[index] = arr1;
+                });
+                console.log("complete_json", complete_json);
+                var product_id = "{{$product_id}}";
+                console.log("product_id", product_id);
+                var fd = new FormData();
+                fd.append( "product_id", product_id );
+                fd.append("title", "{{$product_details['product']['title']}}")
+                fd.append( "complete_json", JSON.stringify(complete_json) );
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+                $.ajax({
+                    url: "{{$domain_name}}/save_printful_product",
+                    type: "POST",
+                    data:fd,
+                    contentType: false,
+                    cache: false,
+                    processData: false,
+                    success: function(data) {
+                       // data = JSON.parse(data);
+                        if(data.status=="success")
+                        {
+                            Swal.fire({
+                                title: '<strong class="proof_popup_title">Product Edit</strong>',
+                                icon: 'success',
+                                html: ' <p style="text-align: center;">Your Product has been Saved with all placements.</p>',
+                                confirmButtonText: 'Okay',
+                                allowOutsideClick: true,
+                                showCloseButton: false,
+                                allowOutsideClick: false,
+                                }).then((result) => {
+                                        if (result.isConfirmed) {
+                                         window.location = "{{$domain_name}}/view_uploaded_products"
+                                        }
+                                })
                         }
                     }
                 });
-            }
-        }
-        $(".gallery").change(function(){
-            readURL_gallery(this);
-        });
-        $(document).on("click", ".saveproduct", function(e){
-            var complete_json = {};
-            $(".jcrop-holder img.js-qv-product-cover.product_image").each(function(index, value){
-                var src = $(value).attr("src");
-                var  = $(value).attr("inner_canvas");
-                var arr1 = {};
-                arr1['src'] = src;
-                arr1['inner_canvas'] = inner_canvas;
-                complete_json[index] = arr1;
             });
-            console.log("complete_json", complete_json);
-            var product_id = "{{$product_id}}";
-            console.log("product_id", product_id);
-            var fd = new FormData();
-            fd.append( "product_id", product_id );
-            fd.append("title", "{{$product_details['product']['title']}}")
-            fd.append( "complete_json", JSON.stringify(complete_json) );
-            $.ajaxSetup({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                }
-            });
-            $.ajax({
-                url: "{{$domain_name}}/save_printful_product",
-                type: "POST",
-                data:fd,
-                contentType: false,
-                cache: false,
-                processData: false,
-                success: function(data) {
-                   // data = JSON.parse(data);
-                    if(data.status=="success")
-                    {
-                        Swal.fire({
-                            title: '<strong class="proof_popup_title">Product Edit</strong>',
-                            icon: 'success',
-                            html: ' <p style="text-align: center;">Your Product has been Saved with all placements.</p>',
-                            confirmButtonText: 'Okay',
-                            allowOutsideClick: true,
-                            showCloseButton: false,
-                            allowOutsideClick: false,
-                            }).then((result) => {
-                                    if (result.isConfirmed) {
-                                     window.location = "{{$domain_name}}/view_uploaded_products"
-                                    }
-                            })
-                    }
-                }
-            });
-        });
-
-});
-
-window.product_details = @json($product_details);
-console.log("product_details", product_details);
-
-</script>
+    });
+    window.product_details = @json($product_details);
+    console.log("product_details", product_details);
+    </script>
 @endsection
